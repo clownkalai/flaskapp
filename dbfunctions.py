ac9914f54b37707a33ee60db7c07d7394d18be09
clownkalai <47328397+clownkalai@users.noreply.github.com>
2023-10-10 13:24:27 +0530

    Update dbfunctions.py

--- dbfunctions.py
+++ dbfunctions.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 
-from databaseConfig  import database_username,database_password,databasename
+
 
 def dbconnect():
         #mongodb connection string
@@ -20,21 +20,24 @@ def find_latest_in_collection_sort(collection,sort_arr):
         return result
 
 def find_all_collection(collection):
-        db = dbconnect()
+	db = dbconnect()
 	col = db[collection]
 	cursor = col.find({},{'_id':0})
 	result = [item for item in cursor]
 	return result 
 
 def find_one_in_collection(collection,arr):
+        print(collection,arr)
         db=dbconnect()
         col = db[collection]
         cursor = col.find(arr,{'_id':0})
         result = [item for item in cursor]
+        print(result)
         return result
 
 
 def save_collection(collection,arr):
+
         db = dbconnect()
         coll = db[collection]
         data = coll.insert_one(arr)
@@ -46,14 +49,18 @@ def find_and_filter(collection,arr,ary):
         cursor = coll.find(arr,ary)
         result = [item for item in cursor]
         return result
+
 def find_in_collection(collection,arr):
+        
         db = dbconnect()
         coll = db[collection]
         cursor = coll.find(arr,{'_id':0})
         result = [item for item in cursor]
+        
         return result
 
-def update_collection(collection,arr,ary):  
+def update_collection(collection,arr,ary):
+      
         db = dbconnect()
         coll = db[collection]
         data = coll.update_one(arr,ary)
