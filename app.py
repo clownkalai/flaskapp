b0760757a8b88983c0cc39103cfd898b38ef8391
clownkalai <47328397+clownkalai@users.noreply.github.com>
2023-10-10 13:11:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,17 +4,7 @@ from pymongo import MongoClient
 app = Flask(__name__)
 
 
-
-def dbconnection():
-    connection_string = "mongodb+srv://kalaiyarasan:kjqYYemV2V0R5RE6@cluster0.7g1bffo.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp"
-    client = MongoClient(connection_string)
-    db = client["Crud_2023"]
-    return db
-
 @app.route('/')
 def hello_world():
-    db = dbconnection()
-    col = db["userInfo"]
-    cursor = col.find({},{'_id':0})
-    result = [item for item in cursor]
+    result="Hi am kalaiyarasan, happy to see you"
     return result
