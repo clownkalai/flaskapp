32ef90851cb3548020e9df3c5210f6ddc46bae6c
clownkalai <47328397+clownkalai@users.noreply.github.com>
2023-10-10 13:03:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ def dbconnection():
     connection_string = "mongodb+srv://kalaiyarasan:kjqYYemV2V0R5RE6@cluster0.7g1bffo.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp"
     client = MongoClient(connection_string)
     db = client["Crud_2023"]
+    return db
 
 @app.route('/')
 def hello_world():
