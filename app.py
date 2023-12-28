08a6e768e7fda49afde5f6cf2854c0c625193106
clownkalai <47328397+clownkalai@users.noreply.github.com>
2023-12-28 17:17:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -209,4 +209,4 @@ def unauthorized_error_handler(error):
         response.headers[header] = value
     return response
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=80,debug=True)
+    app.run(host="0.0.0.0",port=80)
