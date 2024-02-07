7076892c484d0070a59bbb59647e266d929cbb86
clownkalai <47328397+clownkalai@users.noreply.github.com>
2024-02-07 22:23:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 app = Flask(__name__)
+CORS(app)
 app.config['JWT_SECRET_KEY'] = '09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7' 
 jwt = JWTManager(app)
 
